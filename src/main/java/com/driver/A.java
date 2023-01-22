5b250e2bc4235a0d905875f1c6c774b247bfecd8
Abdul Mukheed <you@example.com>
2023-01-22 20:15:12 +0530

    Method overriding 3

---
+++ src/main/java/com/driver/A.java
@@ -0,0 +1,16 @@
+package com.driver;
+
+class A{
+
+    String meth(){
+        return "Invoking method from class A";
+    }
+}
+class B extends A{
+    String meth(){
+        return "Method is overridden in Extendend class B";
+    }
+    String callAMeth(){
+        return super.meth();
+    }
+}
--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -7,17 +7,3 @@ public class Main {
       System.out.println(obj.meth());
   }
 }
-class A{
-
-    String meth(){
-        return "Invoking method from class A";
-    }
-}
-class B extends A{
-    String meth(){
-        return "Method is overridden in Extendend class B";
-    }
-    String callAMeth(){
-        return super.meth();
-    }
-}
