dc77fda82514eee92e4f2fc30bbc40f7144921f9
Abdul Mukheed <you@example.com>
2023-01-22 20:08:38 +0530

    Method overriding 2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,7 +3,7 @@ package com.driver;
 public class Main {
   public static void main(String[] ar){
       B obj = new B();
-      System.out.println(obj.callAMeth());
+      //System.out.println(obj.callAMeth());
       System.out.println(obj.meth());
   }
 }
