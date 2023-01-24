60c2df8ad11143db6a686734675768b5b532976e
Abdul Mukheed <you@example.com>
2023-01-24 10:04:24 +0530

    Method overriding 5

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -7,7 +7,7 @@ public class Main {
             return "Invoking method from class A";
         }
     }
-    static class B extends A{
+    public static class B extends A{
         String meth(){
             return "Method is overridden in Extendend class B";
         }
